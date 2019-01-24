8ae22ce7db051b9f393ff50ded51c2ae0269ef25
Jerpp <42327185+jerpp@users.noreply.github.com>
2019-01-24 08:41:30 -0800

    Create Vector3.java

---
+++ cherrytwist/vector/Vector3.java
@@ -0,0 +1,35 @@
+package cherrytwist.vector;
+
+public class Vector3 {
+	private int x, y, z;
+
+	public Vector3(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/**
+	 * returns the x value of this vector.
+	 * @return x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * returns the y value of this vector.
+	 * @return y
+	 */
+	public int getY() {
+		return y;
+	}
+	
+	/**
+	 * returns the z value of this vector.
+	 * @return z
+	 */
+	public int getZ() {
+		return z;
+	}
+}
