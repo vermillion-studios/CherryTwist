66f3c8f9b62a1374c9a4a62d34f5e25a12e7ed8b
Jerpp <42327185+jerpp@users.noreply.github.com>
2019-01-24 11:53:15 -0800

    Update CherryTwist.java

--- cherrytwist/CherryTwist.java
+++ cherrytwist/CherryTwist.java
@@ -1,14 +1,11 @@
 package cherrytwist;
 
 public class CherryTwist {
-	private static String version = "1.01.2";
+	private static String version = "1.01.1";
 	
 	public static String getVersion() {
 		return version;
 	}
-	
-	public static void main(String[] args) {
-		System.out.println("[CherryTwist v" + version + "] library has been loaded.");
-	}
+
 }
 
