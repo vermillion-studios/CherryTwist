5e046e3217911b24908bdf6b10be5a9a825fb431
Jerpp <42327185+jerpp@users.noreply.github.com>
2019-01-24 08:41:15 -0800

    Create Vector2.java

---
+++ cherrytwist/vector/Vector2.java
@@ -0,0 +1,26 @@
+package cherrytwist.vector;
+
+public class Vector2 {
+	private int x, y;
+
+	public Vector2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * returns the x value of this vector.
+	 * @return x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * returns the y value of this vector.
+	 * @return y
+	 */
+	public int getY() {
+		return y;
+	}
+}
